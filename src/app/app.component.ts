2772cdfabdebe4b81381a261d46ed4ccebadc215
Vutha Phai <vuthaphai@Vuthas-MacBook-Pro.local>
2025-02-23 16:27:58 +0700

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ToolbarComponent } from './pages/colors-quiz/components/toolbar/toolbar.component';
-import { CartComponent } from './pages/signals-shop/components/cart/cart.component';
-import { ItemsListComponent } from './pages/signals-shop/components/items-list/items-list.component';
 import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
-  imports: [SharedModule, ToolbarComponent, ItemsListComponent, CartComponent],
+  imports: [SharedModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/signals-shop/components/items-list/view-model/product-item.vm.ts
+++ src/app/pages/signals-shop/components/items-list/view-model/product-item.vm.ts
@@ -1,5 +1,6 @@
-import { ALL_PRODUCTS } from "../../../data/all-products";
-import { Product } from "../../../models/product.model";
+
+import { ALL_PRODUCTS } from "@app/pages/signals-shop/data/all-products";
+import { Product } from "@app/pages/signals-shop/types/product.model";
 
 export interface ProductItemVm extends Product {
     readonly quantity: number;
@@ -9,6 +10,6 @@ export interface ProductItemVm extends Product {
 export const sampleProductItems: ProductItemVm[] = ALL_PRODUCTS
     .slice(0, 5)
     .map((product, index) => ({
-        ...product, 
+        ...product,
         quantity: index % 3
     }))
